fed04add10f88e46c2a6f7dc1b4946478fb02168
troylelandshields <troy.leland.shields@gmail.com>
2020-02-10 21:13:05 -0700

    button that adds images

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/gopherjs/gopherjs/js"
+import (
+	"strconv"
+	"time"
+
+	"github.com/gopherjs/gopherjs/js"
+	"honnef.co/go/js/dom"
+)
 
 func main() {
-	js.Global.Get("document").Call("write", "Hello world!")
+	js.Global.Get("console").Call("log", "Hey")
+	document := dom.GetWindow().Document()
+
+	btn := document.GetElementByID("btn")
+	btn.AddEventListener("click", false, func(event dom.Event) {
+		img := document.CreateElement("img").(*dom.HTMLImageElement)
+		img.Src = "https://www.thispersondoesnotexist.com/image?" + strconv.Itoa(int(time.Now().Unix()))
+		img.Height = 250
+		document.GetElementByID("container").AppendChild(img)
+	})
 }
