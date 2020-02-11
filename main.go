731329bd7a85c910d10f286f7831c2cc532b3914
troylelandshields <troy.leland.shields@gmail.com>
2020-02-10 21:52:55 -0700

    do it with vecty

--- main.go
+++ main.go
@@ -1,26 +1,77 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 
 	"github.com/gopherjs/gopherjs/js"
-	"honnef.co/go/js/dom"
+	"github.com/gopherjs/vecty"
+	"github.com/gopherjs/vecty/elem"
+	"github.com/gopherjs/vecty/event"
 )
 
 func main() {
 	js.Global.Get("console").Call("log", "Hey")
-	document := dom.GetWindow().Document()
 
-	btn := document.GetElementByID("btn")
-	btn.AddEventListener("click", false, func(event dom.Event) {
-		var x string
-		img := document.CreateElement("img").(*dom.HTMLImageElement)
-		img.Src = "https://www.thispersondoesnotexist.com/image?" + strconv.Itoa(int(time.Now().Unix()))
-		img.Height = 250
+	vecty.SetTitle("Hello Vecty!")
+	vecty.RenderBody(&PageView{})
+}
 
-		panic("Oops")
+// PageView is our main page component.
+type PageView struct {
+	vecty.Core
+}
 
-		document.GetElementByID("container").AppendChild(img)
-	})
+// Render implements the vecty.Component interface.
+func (p *PageView) Render() vecty.ComponentOrHTML {
+	portraitContainer := &Portraits{}
+	return elem.Body(
+		&Button{
+			onClickHandler: portraitContainer.add,
+		},
+		portraitContainer,
+	)
+}
+
+type Portraits struct {
+	vecty.Core
+
+	portraitURLs []string
+}
+
+func (p *Portraits) add() {
+	fmt.Println("Adding a new portrait")
+	newURL := "https://www.thispersondoesnotexist.com/image?" + strconv.Itoa(int(time.Now().Unix()))
+	p.portraitURLs = append(p.portraitURLs, newURL)
+	vecty.Rerender(p)
+}
+
+func (p *Portraits) Render() vecty.ComponentOrHTML {
+	var portraitIMGs []vecty.MarkupOrChild
+	for _, portraitURL := range p.portraitURLs {
+		portraitIMGs = append(portraitIMGs, elem.Image(
+			vecty.Markup(vecty.Attribute("src", portraitURL)),
+			vecty.Markup(vecty.Attribute("width", 250)),
+		))
+	}
+	return elem.Div(portraitIMGs...)
+}
+
+type Button struct {
+	vecty.Core
+
+	onClickHandler func()
+}
+
+func (b *Button) onClick(event *vecty.Event) {
+	fmt.Println("HERE")
+	b.onClickHandler()
+}
+
+func (b *Button) Render() vecty.ComponentOrHTML {
+	return elem.Button(
+		vecty.Text("Click it"),
+		vecty.Markup(event.Click(b.onClick).PreventDefault()),
+	)
 }
